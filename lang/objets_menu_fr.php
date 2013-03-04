63cb673fffdf9d35856c2c4f0494a8ae07be2fac
abelass <websolutions@mychacra.net>
2013-03-04 19:01:08 +0100

    adaptations pour éléments

--- lang/objets_menu_fr.php
+++ lang/objets_menu_fr.php
@@ -10,12 +10,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'ajouter_lien_objets_menu' => 'Ajouter ce objets_menu',
 
 	// I
-	'icone_creer_objets_menu' => 'Créer un Menu',
-	'icone_modifier_objets_menu' => 'Modifier ce Menu',
+	'icone_creer_objets_element' => 'Créer un élément',
+	'icone_creer_objets_menu' => 'Créer un menu',	
+	'icone_modifier_element_menu' => 'Modifier cet élément',
+	'icone_modifier_objets_menu' => 'Modifier ce menu',	
 	'info_1_objets_menu' => 'Un élémént',
-	'info_aucun_objets_menu' => 'Aucun Menu',
+	'info_aucun_objets_menu' => 'Aucun menu',
 	'info_nb_objets_menus' => '@nb@ éléménts',
-	'info_objets_menus_auteur' => 'Les Menus de cet auteur',
+	'info_objets_menus_auteur' => 'Les menus de cet auteur',
 
 	// L
 	'label_ajouter_element' => 'Ajouter un élément',	
@@ -27,15 +29,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'niveau_menu' => 'Menu niveau',
 	
 	// R
-	'retirer_lien_objets_menu' => 'Retirer ce Menu',
-	'retirer_tous_liens_objets_menus' => 'Retirer tous les Menus',
+	'retirer_lien_objets_menu' => 'Retirer ce menu',
+	'retirer_tous_liens_objets_menus' => 'Retirer tous les menus',
 
 	// T
-	'texte_ajouter_objets_menu' => 'Ajouter un Menu',
+	'texte_ajouter_objets_menu' => 'Ajouter un menu',
 	'texte_changer_statut_objets_menu' => 'Ce menu est :',
 	'texte_creer_associer_objets_menu' => 'Créer et associer un menu',
 	'titre_langue_objets_menu' => 'Langue de ce menu',
 	'titre_logo_objets_menu' => 'Logo de ce menu',
+	'titre_nouveau_element' => 'Nouveau élémént',	
 	'titre_objets_menu' => 'Menu',
 	'titre_objets_menus' => 'Menus',
 	'titre_objets_menus_rubrique' => 'Menus de la rubrique',
